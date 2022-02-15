40fe8100c0fcc76691b1b21e3ff31e9edb75aea3
S Sharanya Bharghavi <ssharanyab@gmail.com>
2022-02-15 23:38:05 +0530

    splash screen added

---
+++ app/src/main/java/com/example/cse225ca1sharanya/Homepage.kt
@@ -0,0 +1,11 @@
+package com.example.cse225ca1sharanya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Homepage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage2)
+    }
+}
---
+++ app/src/main/java/com/example/cse225ca1sharanya/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.cse225ca1sharanya
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        Handler().postDelayed({
+        val intent = Intent(this@MainActivity,Homepage::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
