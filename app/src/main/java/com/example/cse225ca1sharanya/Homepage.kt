d3bfed9e5a6327b5c5e1160efc2f7ebbb1891ee5
S Sharanya Bharghavi <ssharanyab@gmail.com>
2022-02-17 16:56:13 +0530

    commit

--- app/src/main/java/com/example/cse225ca1sharanya/Homepage.kt
+++ app/src/main/java/com/example/cse225ca1sharanya/Homepage.kt
@@ -1,38 +1,66 @@
 package com.example.cse225ca1sharanya
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.*
-
+import android.view.LayoutInflater
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintSet
 
 class Homepage : AppCompatActivity() {
     lateinit var receiver:AeroplaneReceiver
     lateinit var ratingBar: RatingBar
     lateinit var button: Button
     lateinit var textView: TextView
+    lateinit var buttonforcus: Button
+    lateinit var sw_wrapper : ScrollView
+    lateinit var progressBar: ProgressBar
+//    @RequiresApi(Build.VERSION_CODES.M)
+    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage2)
-        val toolbar = findViewById<View>(R.id.toolbar) as androidx.appcompat.widget.Toolbar
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         ratingBar = findViewById(R.id.ratingBar)
         ratingBar.numStars = 5
         button = findViewById(R.id.buttonCheck)
-        textView = findViewById(R.id.textView)
+        textView = findViewById(R.id.rateText)
+        sw_wrapper =  findViewById(R.id.scroll)
+    progressBar = findViewById(R.id.progressBar)
+        var parentLayout=findViewById<LinearLayout>(R.id.parentLayout)
+
+        sw_wrapper.setOnScrollChangeListener { view, i, i2, i3, i4 ->
+            val  scrollLen = sw_wrapper.getChildAt(0).height - sw_wrapper.height
+            progressBar.apply {
+                max = scrollLen
+                progress = i2
+            }
+        }
+
+
+
         ratingBar.onRatingBarChangeListener =
             RatingBar.OnRatingBarChangeListener { _, rating, _ ->
-                Toast.makeText(
-                    this@Homepage, "Stars: " +
-                            rating.toInt(), Toast.LENGTH_SHORT
-                ).show()
+
             }
         button.setOnClickListener {
             textView.text = "You have got " + ratingBar.rating.toInt()+ " stars"
+            Toast(this).apply {
+
+                duration = Toast.LENGTH_LONG
+                setGravity(Gravity.TOP,0,250)
+                view = layoutInflater.inflate(R.layout.custom_toast,parentLayout)
+            }.show()
         }
         receiver = AeroplaneReceiver()
         IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
